12c190cc5865f193a612d21c3a4c9e6e7b55f057
jeroenUpowa <jeroen@upowa.org>
2017-09-18 15:08:50 +0200

    Add files via upload
    Commit code

---
+++ LORACommunication/Debug/CommunicationModule.cpp.d
@@ -0,0 +1,31 @@
+C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\CommunicationModule.cpp.o: \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\CommunicationModule.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\arduino15\packages\adafruit\hardware\avr\1.4.9\variants\feather32u4/pins_arduino.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic/lmic.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic/oslmic.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic/config.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic/hal.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\arduino-lmic-master\src/lmic/lorabase.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\Debug/debug.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\communication.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\task_scheduler.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\storage_manager.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\sampling_task.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\reporting_task.h
---
+++ LORACommunication/Debug/gsm_communication.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\Jeroen\ Gillis\AppData\Local\Temp\VMBuilds\CommunicationModule\adafruit_adafruit_avr_feather32u4\Debug\gsm_communication.cpp.o: \
+ C:\Users\Jeroen\ Gillis\Source\Repos\moduleComm\GSMCommunication\gsm_communication.cpp \
+ C:\Users\Jeroen\ Gillis\Source\Repos\moduleComm\GSMCommunication\communication.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
+ C:\Users\Jeroen\ Gillis\AppData\Local\arduino15\packages\adafruit\hardware\avr\1.4.9\variants\feather32u4/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Jeroen\ Gillis\Documents\Arduino\libraries\Debug/debug.h
